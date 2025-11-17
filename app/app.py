c0f66edaa270f0844ba9357495e4b118a6ba424f
sasassa123 <160864462+sasassa123@users.noreply.github.com>
2025-11-16 21:05:41 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -377,6 +377,7 @@ def logout():
     return redirect("/login")
 
 if __name__ == "__main__":
-    print("✅ Backend rodando em http://localhost:5000")
+    print(" Backend rodando em http://localhost:5000")
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
